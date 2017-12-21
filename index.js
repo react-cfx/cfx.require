0cb62970ecfbe2a809e79c2d3c3053e9a5318f35
FooTearth <footearth@gmail.com>
2017-12-21 16:28:09 +0800

    merge default and mata use once require

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { gdf } from './src/index.coffee';
-import _require from './src/index.coffee';
+import _require, { gdf } from './src/index.coffee';
 
 export { gdf };
 export default _require;
